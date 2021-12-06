b43ed09c8288a3f9a681d08111d104083cd4be5d
Frank Fenton <echelon.iiii@gmail.com>
2021-12-06 16:43:17 +1100

    ✨ Implement day 6 part 2

---
+++ day-6/part2.ts
@@ -0,0 +1,14 @@
+import { LanternfishPopulation } from './LanternfishPopulation.js'
+import { loadInput } from './load.js'
+
+const input = loadInput()
+
+const population = new LanternfishPopulation(input)
+console.log('initial population', population.totalPopulation)
+console.log('population in day buckets', population.bucketsAsString)
+
+for (let day = 1; day <= 256; day += 1) {
+  population.tickOneDay()
+  console.log(`population on day ${day} is ${population.totalPopulation}`)
+  // console.log('population in day buckets', population.bucketsAsString)
+}
