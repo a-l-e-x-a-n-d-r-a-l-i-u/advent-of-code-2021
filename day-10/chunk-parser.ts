c77548bdb129c39dc7e58552e49a3db618d0ad7d
Frank Fenton <echelon.iiii@gmail.com>
2021-12-10 17:08:00 +1100

    ♻ Refactor part 1 so part two will work

---
+++ day-10/chunk-parser.ts
@@ -0,0 +1,29 @@
+import { Char } from './load'
+
+export class ChunkError extends Error {
+  constructor(message: string, public readonly badChar: Char) {
+    super(message)
+    this.name = 'ChunkError'
+  }
+}
+
+export function checkLine(line: Char[]): Char | null {
+  const stack: Char[] = []
+  for (const char of line) {
+    if (['[', '(', '{', '<'].includes(char)) {
+      stack.push(char)
+    } else {
+      const lastOpening = stack.pop()
+      if (
+        (char === ')' && lastOpening !== '(') ||
+        (char === ']' && lastOpening !== '[') ||
+        (char === '}' && lastOpening !== '{') ||
+        (char === '>' && lastOpening !== '<')
+      ) {
+        //illegal
+        throw new ChunkError('bad chunk', char)
+      }
+    }
+  }
+  return null
+}
--- day-10/part1.ts
+++ day-10/part1.ts
@@ -1,39 +1,24 @@
-import { Char, loadInput } from './load.js'
+import { checkLine, ChunkError } from './chunk-parser.js'
+import { loadInput } from './load.js'
 
 const input = loadInput()
 
-function checkLine(line: Char[]): Char | null {
-  const stack: Char[] = []
-  for (const char of line) {
-    if (['[', '(', '{', '<'].includes(char)) {
-      stack.push(char)
-    } else {
-      const lastOpening = stack.pop()
-      if (
-        (char === ')' && lastOpening !== '(') ||
-        (char === ']' && lastOpening !== '[') ||
-        (char === '}' && lastOpening !== '{') ||
-        (char === '>' && lastOpening !== '<')
-      ) {
-        //illegal
-        return char
-      }
-    }
-  }
-  return null
-}
-
 let totalScore = 0
 for (const line of input) {
-  const firstIllegal = checkLine(line)
-  if (firstIllegal === ')') {
-    totalScore += 3
-  } else if (firstIllegal === ']') {
-    totalScore += 57
-  } else if (firstIllegal === '}') {
-    totalScore += 1197
-  } else if (firstIllegal === '>') {
-    totalScore += 25137
+  try {
+    checkLine(line)
+  } catch (error) {
+    if (error instanceof ChunkError) {
+      if (error.badChar === ')') {
+        totalScore += 3
+      } else if (error.badChar === ']') {
+        totalScore += 57
+      } else if (error.badChar === '}') {
+        totalScore += 1197
+      } else if (error.badChar === '>') {
+        totalScore += 25137
+      }
+    }
   }
 }
 
