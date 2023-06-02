15314a1569d43e557273c74163ac965c95820c89
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2023-06-02 18:22:32 +1000

    Combined part 1 and 2 into one file

--- day-1/common.ts
+++ day-1/common.ts
@@ -1,16 +1,14 @@
 import { readFileSync } from 'fs'
 
 //Cleaning the data
-export function loadInput(): number[] {
-  return readFileSync('input', { encoding: 'utf-8' })
+const cleanInput: number[] = readFileSync('input', { encoding: 'utf-8' })
     .split('\n')
     .map((line: string) => line.trim())
     .filter((line: string) => line.length > 0)
     .map(Number)
-}
 
-//Comparing numbers
-export function compareDepths(arr: number[]): number[] {
+//Comparing depths
+function compareDepths(arr: number[]): number[] {
   const result: number[] = [];
 
   for (let i = 1; i < arr.length; i++) {
@@ -19,3 +17,26 @@ export function compareDepths(arr: number[]): number[] {
   }
   return result
 }
+
+//Comparing windows
+function compareWindows(arr: number[]): number[] {
+  const result: number[] = [];
+
+  for (let i = 2; i < arr.length; i++) {
+    const comparison: number = arr[i-1]+arr[i]+arr[i+1] > arr[i-2]+arr[i-1]+arr[i] ? 1 : 0;
+    result.push(comparison)
+  }
+  return result
+}
+
+//Callback
+function getSumTotal(arr: number[]): number {
+  return arr.reduce((accumulator, currentNumber) => accumulator + currentNumber, 0);
+}
+
+//Results
+const depthComparisonResult: number[] = compareDepths(cleanInput)
+const windowComparisonResult: number[] = compareWindows(cleanInput)
+
+console.log('part 1: total increases:', getSumTotal(depthComparisonResult))
+console.log('part 2: total increases:', getSumTotal(windowComparisonResult))
