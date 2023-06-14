9352b7f41292caf150cacc56038b867feb09ac7c
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2023-06-14 22:08:20 +1000

    Data is all cleeeeean!!!!!

--- day-3/common.ts
+++ day-3/common.ts
@@ -1,23 +1,34 @@
 import { readFileSync } from 'fs'
 
-export function loadInput(): number[] {
+const binaryChecker: RegExp = /^[01]+$/;
+
+export function loadInput(): string[] {
+  
   const dataset = readFileSync('input', { encoding: 'utf-8' })
     .split('\n')
     .map((line) => line.trim())
     .filter((line) => line.length > 0)
-    .map(Number);
     
     if (dataset.length === 0) {
-      throw new Error('Dataset is emptry, could not parse')
-    }
+      throw new Error('Dataset is empty, could not parse')
+    }  
 
-    if (!dataset.every((line) => line.toString().length === dataset[0].toString().length)) {
+    //Check that all lines in the dataset are of the same length
+    if (!dataset.every((line) => line.length === dataset[0].length)) {
       for (let i = 1; i < dataset.length; i++) {
-        if (dataset[i].toString().length !== dataset[0].toString().length) {
-          console.log(i, ':', dataset[i]);
+        if (dataset[i].length !== dataset[0].length) {
+          console.log(i, ':', dataset[i])
+          throw new Error('Line(s) in this dataset do not have the same length')
         }
       }
-      throw new Error('Elements in this dataset do not have the same length')
+    }
+
+    //Check that all lines contain only 1s or 0s
+    for (const line of dataset) {
+      if (!binaryChecker.test(line)) {
+        console.log(line)
+        throw new Error('Line(s) in this dataset contain characters other than 1s or 0s')
+      }
     }
 
     return dataset
