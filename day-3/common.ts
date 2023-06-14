98f96a0a003b7a5bd914e9770e79dea760d12e8f
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2023-06-14 14:54:54 +1000

    Identified where the data isn't clean

--- day-3/common.ts
+++ day-3/common.ts
@@ -12,6 +12,11 @@ export function loadInput(): number[] {
     }
 
     if (!dataset.every((line) => line.toString().length === dataset[0].toString().length)) {
+      for (let i = 1; i < dataset.length; i++) {
+        if (dataset[i].toString().length !== dataset[0].toString().length) {
+          console.log(i, ':', dataset[i]);
+        }
+      }
       throw new Error('Elements in this dataset do not have the same length')
     }
 
