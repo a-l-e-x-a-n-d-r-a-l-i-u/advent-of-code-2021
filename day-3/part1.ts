3131f3b970aeb5255e3a553d2525f33b887fb3ff
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2023-06-17 20:20:59 +1000

    Integrated Frank's function into my own

--- day-3/part1.ts
+++ day-3/part1.ts
@@ -2,56 +2,38 @@ import { loadInput } from './common.js'
 
 const dataset: string[] = loadInput()
 
-const totalLines = dataset.length
-const totalDigitsPerLine = dataset[0].length
+function createBinaryArray(array: string[]) {
+  const halfwayPoint = array.length / 2
+  const numberOfDigits = array[0].length
 
-function createResultNumber(array: string[]) {
-  const countsOfDigitPositions = {};
+  const countOfZeros: number[] = Array(numberOfDigits).fill(0);
     
-  // Count the occurrence of each digit position
+  // Count the occurrence of zeros at each digit position
   for (const line of array) {
     const digits = line.split("");
     for (let i = 0; i < digits.length; i++) {
-      countsOfDigitPositions[i] = {};
-      countsOfDigitPositions[i][digits[i]] = (countsOfDigitPositions[i][digits[i]] || 0) + 1;
-    }
-  }
-    
-    // Find the most common digit for each position
-    const resultDigits = [];
-    for (const position in countsOfDigitPositions) {
-      const positionCounts = countsOfDigitPositions[position];
-      let mostCommonDigit = null;
-      let highestCount = -1;
-      for (const digit in positionCounts) {
-        if (positionCounts[digit] > highestCount) {
-          mostCommonDigit = digit;
-          highestCount = positionCounts[digit];
+      if (digits[i] === "0") {
+        countOfZeros[i]++
+
+        // You don't necessarily need to break the counting of zeros early, but it just saves computational power if the dataset is big
+        if (countOfZeros[i] > halfwayPoint) {
+          break
         }
+      } else {
+        return
       }
-      resultDigits.push(mostCommonDigit);
     }
-    
-    // Combine the digits to form the resulting number
-    const resultNumber = parseInt(resultDigits.join(""), 2);
-    return resultNumber.toString(2).padStart(3, "0");
   }
-  
-  // Example usage:
-  const result = createResultNumber(dataset);
-  
-  console.log(result);
 
+  console.log(countOfZeros)
 
-public get currentGammaAndEpsilon(): [number, number] {
-  const halfwayPoint = this.totalLines / 2
-  const gammaArray: number[] = []
-  const epsilonArray: number[] = []
-  for (const count of this.currentCount) {
-    const moreOnesThanZeros = count > halfwayPoint
-    gammaArray.push(moreOnesThanZeros ? 1 : 0)
-    epsilonArray.push(moreOnesThanZeros ? 0 : 1)
-  }
-  // console.log('binary of current count', gammaArray.join(''))
-  return [parseInt(gammaArray.join(''), 2), parseInt(epsilonArray.join(''), 2)]
+  const gammaArray: number[] = countOfZeros.map((number) => number > halfwayPoint ? 0 : 1)
+  const epsilonArray: number[] = countOfZeros.map((number) => number < halfwayPoint ? 0 : 1)
+
+  const gammaResult = parseInt(gammaArray.join(""), 2)
+  const epsilonResult = parseInt(epsilonArray.join(""), 2)
+
+  console.log('gamma:', gammaResult, 'epsilon', epsilonResult)
 }
+
+createBinaryArray(dataset)
