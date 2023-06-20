6c14988692424aade3f9cb858fbe9b711d26b25f
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2023-06-20 14:32:01 +1000

    Rotation step was completely unnecessary given it's already iterating through each line to count instances of 0s or 1s at each digit position

--- day-3/part2.ts
+++ day-3/part2.ts
@@ -2,100 +2,33 @@ import { loadInput } from './common.js'
 
 const dataset: string[] = loadInput()
 
-function rotateArray(array: string[]): string[] {
-  const columns = array[0].length;
-  const rotatedWorkingArray = Array.from({ length: columns }, ((_, i) => array.map(string => string[i]))); // Rotates elements, but the result is an array of arrays of individual characters
-  const rotatedArray = rotatedWorkingArray.map(column => column.join('')) // Combines the split characters into a cohesive string
-  return rotatedArray;
-}
-
-function countDigitOccurences(array: string[], findMax: boolean) { 
-  const count: number[] = [0, 0]; // Count of 0s and 1s at each digit position
-  for (const element of array) {
-    count[parseInt(element)]++; // Increment count at current digit position
-  }
-  const result: string = findMax ? (count[0] > count[1] ? 0 : 1) : (count[0] < count[1] ? 0 : 1)
-  return result
-}
-
 function filterByCommonDigits(array: string[], findMax: boolean): string[] {
-  const criteria = "";
-  const filteredArray = array.filter((str) => str.startsWith(criteria))
+  let criteria = "";
+  let filteredArray = array.filter((str) => str.startsWith(criteria))
 
-  while filteredArray.length > 1 {
-    for (let i = 0; i < array[0].length; i++) {
-      const rotatedArray = rotateArray(filteredArray);
-      const nextDigitInCriteria = countDigitOccurences(rotatedArray, findMax);
-      criteria.concat(nextDigitInCriteria)
-
+  while (filteredArray.length > 1) {
+    const count: number[] = [0, 0]; // Count of 0s and 1s at each digit position
+    for (const element of array) {
+      count[parseInt(element)]++; // Increment count at current digit position
     }
+
+    const nextDigit: number = findMax ? (count[0] > count[1] ? 0 : 1) : (count[0] < count[1] ? 0 : 1);
+    criteria += nextDigit.toString()
+    filteredArray = filteredArray.filter((str) => str.startsWith(criteria));
   }
 
   return filteredArray;
 }
 
-  let rotatedArray = rotatedArray(array);
-  let filteredArray = array.filter
-  let criteria = "a".concat("b");
+const oxygenBinary = filterByCommonDigits(dataset, true)
+const co2Binary = filterByCommonDigits(dataset, false)
 
-  for (const line of rotatedArray) {
-    count[parseInt(line[i])]++; // Increment count at current digit position
-  }
+// Parse the binary numbers into integers
+const oxygenResult = parseInt(oxygenBinary.join(""), 2)
+const co2Result = parseInt(co2Binary.join(""), 2)
 
+console.log('oxygen:', oxygenResult)
+console.log('co2:', co2Result)
 
-
-  const lastDigit = array[0].length - 1
-
-  let i = 0;
-  while (array.length > 1 && i <= lastDigit) {
-//  while (array.length > 0) {
-    const count: number[] = [0, 0]; // Count of 0s and 1s at each digit position
-
-    for (const line of array) {
-      count[parseInt(line[i])]++; // Increment count at current digit position
-    }
-
-    const targetFrequency = findMax ? Math.max(...count) : Math.min(...count)
-    const targetDigit = count.indexOf(targetFrequency)
-
-    array = array.filter((line) => parseInt(line[i]) === targetDigit)
-
-    if (array.length === 2) {
-      array.sort((a, b) => {
-        const comparison = parseInt(a[lastDigit]) - parseInt(b[lastDigit]);
-        return findMax ? comparison : -comparison;
-      });
-    }
-
-    i++;
-  }
-
-  return array[0]
-}
-
-filterByCommonDigits(dataset, true)
-
-
-
-
-
-const allLines = readFileSync('input', { encoding: 'utf-8' })
-  .split('\n')
-  .filter((line, _, array) => line.length === array[0].length)
-function filterToSignificantBits(lines: string[], most: boolean, index: number): string[] {
-  const count = lines.reduce((total, currentLine) => (currentLine[index] === '1' ? total + 1 : total), 0)
-  let wantedBit: '1' | '0'
-  if (count >= lines.length / 2) {
-    wantedBit = most ? '1' : '0'
-  } else {
-    wantedBit = most ? '0' : '1'
-  }
-  return lines.filter((line) => line[index] === wantedBit)
-}
-
-function filterToOneResult(most: boolean): string {
-  let potentialLines = allLines
-  let i = 0
-  while (potentialLines.length > 1) {
-    potentialLines = filterToSignificantBits(potentialLines, most, i)
-    i += 1
+const powerConsumptionRating = oxygenResult * co2Result
+console.log('power:', powerConsumptionRating)
