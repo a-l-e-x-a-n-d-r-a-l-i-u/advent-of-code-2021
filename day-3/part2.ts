08b57f0d31f2f2356e17cdf8e0d16dc485f1b5f4
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2023-06-21 22:29:22 +1000

    Yesss it works!

--- day-3/part2.ts
+++ day-3/part2.ts
@@ -4,53 +4,36 @@ const dataset: string[] = loadInput()
 
 
 
-function filterToSignificantBits(lines: string[], most: boolean, index: number): string[] {
-  const count = lines.reduce((total, currentLine) => (currentLine[index] === '1' ? total + 1 : total), 0)
-  let wantedBit: '1' | '0'
-  if (count >= lines.length / 2) {
-    wantedBit = most ? '1' : '0'
-  } else {
-    wantedBit = most ? '0' : '1'
-  }
-  return lines.filter((line) => line[index] === wantedBit)
-}
+function filterByCommonDigits(array: string[], most: boolean): string {
 
-function filterToOneResult(most: boolean): string {
-  let potentialLines = allLines
-  let i = 0
+  let potentialLines = array;
+  let i = 0;
+
   while (potentialLines.length > 1) {
-    potentialLines = filterToSignificantBits(potentialLines, most, i)
-    i += 1
-  }
-}
+    const countOfZeros: number = potentialLines.reduce((total, currentLine) => (currentLine[i] === '0' ? total + 1 : total), 0)
+    const countOfOnes: number = potentialLines.reduce((total, currentLine) => (currentLine[i] === '1' ? total + 1 : total), 0)
 
-
-
-function filterByCommonDigits(array: string[], findMax: boolean): string[] {
-  let criteria = "";
-  let filteredArray = array.filter((str) => str.startsWith(criteria))
-
-  while (filteredArray.length > 1) {
-    const count: number[] = [0, 0]; // Count of 0s and 1s at each digit position
-    for (const element of array) {
-      count[parseInt(element)]++; // Increment count at current digit position
-    }
-
-    const targetDigit: number = count[0] > count[1] ? 0 : 1;
-    criteria += findMax ? targetDigit.toString() : (targetDigit === 0 ? "1" : "0");
-    
-    filteredArray = filteredArray.filter((str) => str.startsWith(criteria));
+    const criterion: '0' | '1' = most ? ( countOfZeros > countOfOnes ? '0' : '1' ) : ( countOfOnes < countOfZeros ? '1' : '0' )
+    potentialLines = potentialLines.filter((line) => line[i] === criterion)
+    // console.log(i, potentialLines)
+    i++
   }
 
-  return filteredArray;
+  if (potentialLines.length === 1) {
+    console.log(potentialLines[0]);
+  }
+
+  return potentialLines[0];
 }
 
 const oxygenBinary = filterByCommonDigits(dataset, true)
 const co2Binary = filterByCommonDigits(dataset, false)
+console.log('oxygen:', oxygenBinary)
+console.log('co2:', co2Binary)
 
 // Parse the binary numbers into integers
-const oxygenResult = parseInt(oxygenBinary.join(""), 2)
-const co2Result = parseInt(co2Binary.join(""), 2)
+const oxygenResult = parseInt(oxygenBinary, 2)
+const co2Result = parseInt(co2Binary, 2)
 
 console.log('oxygen:', oxygenResult)
 console.log('co2:', co2Result)
