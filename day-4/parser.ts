9461158ee5cc36fcb3d47ad868ff97b16e13cdbd
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2023-07-04 15:46:19 +1000

    Turn array of strings into an array of array of strings

--- day-4/parser.ts
+++ day-4/parser.ts
@@ -1,14 +1,6 @@
 import { readFileSync } from 'fs'
 import { BingoBoard } from './BingoBoard.js'
 
-/** STEPS
- * 1. Separate out the first line into a comma separated array
- * 2. Check that first line has minimum 5 elements
- * 
- * 3. Separate out each line break into matrixes
- * 4. Check that each matrix is 5 elements by 5 elements
- */
-
 export function loadInput(): [number[], BingoBoard[]] {
   const inputContents = readFileSync('input', { encoding: 'utf-8' })
   .split(/\n\s*\n/) // Chunk it by empty linespaces with a regular expression -- so inputContents is not a string but an array of strings now
@@ -17,14 +9,16 @@ export function loadInput(): [number[], BingoBoard[]] {
   .trim()
   .split(',')
   .map(Number)
-    
-    if (drawnNumbers.length < 5) {
-      throw new Error('There are not enough drawn numbers to complete this game of bingo')
-    }
+  if (drawnNumbers.length < 5) {
+    throw new Error('There are not enough drawn numbers to complete this game of bingo')
+  }
 
-    
-  const boardsInput = inputContents
-    .slice(firstLineBreak)
+/** STEPS
+ * 3. Separate out each line break into matrixes
+ * 4. Check that each matrix is 5 elements by 5 elements
+ */
+const boardsInput = inputContents.slice(1) // Omit element at index 0
+  .map((str) => str.split(',')) // Turn array of strings into an array of array of strings
     .split('\n\n')
     .map((rawBoard) => rawBoard.trim())
     .filter((rawBoard) => rawBoard.length > 0)
