76574dacc55f7cb1fee61ff7ad0f2b4e3b625b32
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2024-09-27 16:09:50 +1000

    Break the cycle when game is won

--- day-4/part1.ts
+++ day-4/part1.ts
@@ -2,16 +2,23 @@ import { loadInput } from "./parser.js";
 
 const [drawnNumbers, allBoards] = loadInput();
 
+let gameWon = false; // Flag to stop the game once a winning board is found
+
 for (const number of drawnNumbers) {
+  if (gameWon) break;
+
   for (const board of allBoards) {
-      checkBingo(number, board.cells);
+      gameWon = checkBingo(number, board.cells);
+      if (gameWon) break;
   }
 }
 
-function checkBingo(currentNumber: number, matrix: number[][]): void {
+function checkBingo(currentNumber: number, matrix: number[][]): boolean {
   let marker = -1;
   // Include error checks to make sure marker doesn't exist in drawnNumbers and marker doesn't exist in BingoBoard already!
-  // Maybe marker should be declared elsewhere but leave it inside checkBingo for now
+  // Maybe marker should be declared in parser but leave it inside checkBingo for now
+
+  console.log('Checking for', currentNumber)
 
   const hasWonInRows = matrix.some(row => row.every(cell => cell === marker));
   const hasWonInColumns = matrix[0].map((_, index) => matrix.every(row => row[index] === marker)).some(result => result);
@@ -19,7 +26,7 @@ function checkBingo(currentNumber: number, matrix: number[][]): void {
   if (hasWonInRows || hasWonInColumns) {
     console.log('Winning board:', matrix)
     const sumOfRemainingNumbers = matrix.flat().reduce((acc, cell) => {
-        if (cell > marker) {
+        if (cell !== marker) {
           acc = acc + cell
         }
       return acc
@@ -27,8 +34,9 @@ function checkBingo(currentNumber: number, matrix: number[][]): void {
     console.log('Current number:', currentNumber)
     console.log('Sum of remaining numbers:', sumOfRemainingNumbers)
     console.log('Final score:', currentNumber * sumOfRemainingNumbers)
+    return true; // Return true if a board has won
   }
-  console.log(currentNumber, 'checked!')
+  return false; // Return false if no win yet
 }
 
 //Edge case, what if there are multiple winners in for a given current number?
