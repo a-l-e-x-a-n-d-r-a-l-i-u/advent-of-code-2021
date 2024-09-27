0392aaef0b8e1bd0eedb962adb5512c892cc4707
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2024-09-27 16:55:57 +1000

    More concise

--- day-4/part1.ts
+++ day-4/part1.ts
@@ -2,16 +2,10 @@ import { loadInput } from "./parser.js";
 
 const [drawnNumbers, allBoards] = loadInput();
 
-let gameWon = false; // Flag to stop the game once a winning board is found
-
 for (const number of drawnNumbers) {
   console.log('Checking for', number)
-
-  if (gameWon) break;
-
   for (const board of allBoards) {
-      gameWon = checkBingo(number, board.cells);
-      if (gameWon) break;
+      if (checkBingo(number, board.cells) === true) break;
   }
 }
 
@@ -20,23 +14,23 @@ function checkBingo(currentNumber: number, matrix: number[][]): boolean {
   // Include error checks to make sure marker doesn't exist in drawnNumbers and marker doesn't exist in BingoBoard already!
   // Maybe marker should be declared in parser but leave it inside checkBingo for now
 
-  const hasWonInRows = matrix.some(row => row.every(cell => cell === marker));
-  const hasWonInColumns = matrix[0].map((_, index) => matrix.every(row => row[index] === marker)).some(result => result);
+  const hasWon = matrix.some(row => row.every(cell => cell === marker)) // Winning row
+    || matrix[0].map((_, index) => matrix.every(row => row[index] === marker)).some(result => result); // Winning column
 
-  if (hasWonInRows || hasWonInColumns) {
-    console.log('Winning board:', matrix)
-    const sumOfRemainingNumbers = matrix.flat().reduce((acc, cell) => {
-        if (cell !== marker) {
-          acc = acc + cell
-        }
-      return acc
-    }, 0)
-    console.log('Current number:', currentNumber)
-    console.log('Sum of remaining numbers:', sumOfRemainingNumbers)
-    console.log('Final score:', currentNumber * sumOfRemainingNumbers)
-    return true; // Return true if a board has won
+  if (!hasWon) return false;
+
+  const sumOfRemainingNumbers = matrix.flat().reduce((acc, cell) => {
+    if (cell !== marker) {
+        acc = acc + cell
+      }
+    return acc
+  }, 0)
+  
+  console.log('Winning board:', matrix)
+  console.log('Current number:', currentNumber)
+  console.log('Sum of remaining numbers:', sumOfRemainingNumbers)
+  console.log('Final score:', currentNumber * sumOfRemainingNumbers)
+  return true; // Return true if a board has won
   }
-  return false; // Return false if no win yet
-}
 
 //Edge case, what if there are multiple winners in for a given current number?
