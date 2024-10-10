e3ab09cc4bdfe47bb8d051e2ddcfa74c68c51c56
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2024-10-11 10:44:03 +1100

    Perfectly breaks raw data down into parseable code

--- day-5/common.ts
+++ day-5/common.ts
@@ -8,10 +8,11 @@ export function loadInput(): Vector[] {
     .filter((line) => line.length > 0)
   return allLines.map(parseVector)
 }
+
 function parsePoint(pointAsString: string): Point {
   const points = pointAsString.split(',').map(Number)
   if (points.length !== 2) {
-    throw new Error('points can only have two numbers')
+    throw new Error('Points can only have two numbers')
   }
   return new Point(points[0], points[1])
 }
@@ -19,7 +20,7 @@ function parsePoint(pointAsString: string): Point {
 function parseVector(vectorAsString: string): Vector {
   const parts = vectorAsString.split(' -> ')
   if (parts.length !== 2) {
-    throw new Error('vector does not contain two points')
+    throw new Error('Vector does not contain two points')
   }
   const points = parts.map(parsePoint)
   return new Vector(points[0], points[1])
