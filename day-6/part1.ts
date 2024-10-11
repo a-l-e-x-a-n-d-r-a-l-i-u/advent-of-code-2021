8d5dc22aff24242786d47fd5a24428e5461b8c57
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2024-10-11 22:13:54 +1100

    Frank's code sucks in this instance

--- day-6/part1.ts
+++ day-6/part1.ts
@@ -1,14 +1,24 @@
-import { LanternfishPopulation } from './LanternfishPopulation.js'
 import { loadInput } from './load.js'
 
-const input = loadInput()
+// Each value of the array is a fish's number of days left to reproduce
+const initialPopulation: number[] = loadInput()
 
-const population = new LanternfishPopulation(input)
-console.log('initial population', population.totalPopulation)
-console.log('population in day buckets', population.bucketsAsString)
+const processArray = (array: number [], iterations: number) => {
+  let count = 0; // Counter for iterations
 
-for (let day = 1; day <= 80; day += 1) {
-  population.tickOneDay()
-  console.log(`population on day ${day} is ${population.totalPopulation}`)
-  // console.log('population in day buckets', population.bucketsAsString)
-}
+  const intervalId = setInterval(() => {
+      array = array.map(num => (num === 0 ? 6 : num - 1)); // Decrement or reset to 6
+      if (array.some(num => num === 0)) {
+        array.push(8)
+      }
+
+      if (++count >= iterations) {
+          clearInterval(intervalId)
+          console.log(`After ${iterations} days, population is ${array}`);
+          return array
+      }
+  }, 1000); // Repeat every second
+};
+
+const finalArray = processArray(initialPopulation, 80)
+console.log(finalArray)
