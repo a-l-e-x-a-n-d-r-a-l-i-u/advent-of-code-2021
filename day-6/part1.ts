5db19f91a102c543434caff2fec94cd882a07be2
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2024-10-12 10:25:46 +1100

    Abstract the maths away for part 2

--- day-6/part1.ts
+++ day-6/part1.ts
@@ -1,30 +1,30 @@
 import { loadInput } from './load.js'
 
 // Each value of the array is a fish's number of days left to reproduce
-const initialPopulation: number[] = loadInput()
-const testPopulation: number[] = [1, 0, 6, 5]
+const everyFish: number[] = loadInput()
 
-const processArray = (array: number [], iterations: number): Promise<number[]> => {
+export const processArray = (array: number[], iterations: number): Promise<number[]> => {
   return new Promise((resolve) => {
-    let count = 0; // Counter for iterations
-
-    const intervalId = setInterval(() => {
-      array.forEach(num => {
-        if (num === 0) { array.push(9) }
-      }) // So that next day's new fish will start counting down from 8
+    for (let count = 0; count < iterations; count++) { // Counter for iterations
+      const newFish = array.filter(num => num === 0).length; // Count how many 0s
+//      console.log(`Before mapping:`);
+//      console.log(array);
 
       array = array.map(num => (num === 0 ? 6 : num - 1)); // Decrement or reset to 6
-        console.log(array)
-  
-        if (++count >= iterations) {
-            clearInterval(intervalId)
-            console.log(`After ${iterations} days, population is ${array.length}`);
-            resolve(array)
-        }
-    }, 300);
-  })
-}
+//      console.log(`After mapping:`);
+//      console.log(array);
+      
+      for (let i = 0; i < newFish; i++) { // Push a new fish for each zero found
+          array.push(8);
+      }
+//      console.log(`After adding new fish:`);
+      console.log(array);
+    }
+    console.log(`After ${iterations} days, population is ${array.length}`);
+    resolve(array);
+  });
+};
 
-processArray(testPopulation, 5).then(finalArray => {
-  console.log(`There are ${finalArray.length} lanternfish after 80 days`)
+processArray(everyFish, 80).then(finalArray => {
+  console.log(`There are ${finalArray.length} lanternfish after 256 days`)
 });
