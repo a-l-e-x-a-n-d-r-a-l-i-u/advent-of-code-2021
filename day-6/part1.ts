ad70806c9c127869f7395f674c71f6d767bb7c4b
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2024-10-11 22:40:57 +1100

    Made a promise

--- day-6/part1.ts
+++ day-6/part1.ts
@@ -3,22 +3,27 @@ import { loadInput } from './load.js'
 // Each value of the array is a fish's number of days left to reproduce
 const initialPopulation: number[] = loadInput()
 
-const processArray = (array: number [], iterations: number) => {
-  let count = 0; // Counter for iterations
+const processArray = (array: number [], iterations: number): Promise<number[]> => {
+  return new Promise((resolve) => {
+    let count = 0; // Counter for iterations
 
-  const intervalId = setInterval(() => {
+    const intervalId = setInterval(() => {
+      array.forEach(num => {
+        if (num === 0) { array.push(9) }
+      }) // So that next day's new fish will start counting down from 8
+
       array = array.map(num => (num === 0 ? 6 : num - 1)); // Decrement or reset to 6
-      if (array.some(num => num === 0)) {
-        array.push(8)
-      }
+        console.log(array)
+  
+        if (++count >= iterations) {
+            clearInterval(intervalId)
+            console.log(`After ${iterations} days, population is ${array.length}`);
+            resolve(array)
+        }
+    }, 300);
+  })
+}
 
-      if (++count >= iterations) {
-          clearInterval(intervalId)
-          console.log(`After ${iterations} days, population is ${array}`);
-          return array
-      }
-  }, 1000); // Repeat every second
-};
-
-const finalArray = processArray(initialPopulation, 80)
-console.log(finalArray)
+processArray(initialPopulation, 80).then(finalArray => {
+  console.log(`There are ${finalArray.length} lanternfish after 80 days`)
+});
