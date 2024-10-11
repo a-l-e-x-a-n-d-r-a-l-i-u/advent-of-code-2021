805129b4d4e3e77d19cf7dc795a41c044a79caad
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2024-10-12 10:28:53 +1100

    Needs refactoring. I'll park it for tomorrow

--- day-6/test.ts
+++ day-6/test.ts
@@ -1,43 +1,48 @@
-import { loadInput } from './load.js'
+import { loadInput } from './load.js';
 
-// Each value of the array is a fish's number of days left to reproduce
-const everyFish: number[] = loadInput();
-const testPond: number[] = [1, 0, 6, 5]
-
-
-export const dayCounter = (array: number[], iterations: number): Promise<number[]> => {
+// Generic function to process fish population based on the specified counting method
+const simulateFishPopulation = (initialPopulation, iterations, countingMethod) => {
   return new Promise((resolve) => {
-
-    // Keep an array for the number of fish by days left to reproduce
-    const fishByDaysLeft = new Array(9).fill(0);
-    array.forEach(num => {
-      fishByDaysLeft[num]++;
-    });
-
-    // Process array
+    const fishCounts = countingMethod(initialPopulation);
+    
     for (let count = 0; count < iterations; count++) {
-      const reproductiveFish = fishByDaysLeft[0]; // Number of fish that are due to give birth
+      const reproductiveFish = fishCounts[0]; // Number of fish that are due to give birth
 
       // As the day passes, the number of days left to reproduce goes down
-      for (let i = 1; i < fishByDaysLeft.length; i++) {
-        fishByDaysLeft[i - 1] = fishByDaysLeft[i];
+      for (let i = 1; i < fishCounts.length; i++) {
+        fishCounts[i - 1] = fishCounts[i];
       }
 
-      fishByDaysLeft[8] = reproductiveFish;   // New fish get birthed by the reproductive fish
-      fishByDaysLeft[6] += reproductiveFish; // The previously reproductive fish now get added to the 6-day-left fish
+      fishCounts[8] = reproductiveFish;   // New fish get birthed by the reproductive fish
+      fishCounts[6] += reproductiveFish; // The previously reproductive fish now get added to the 6-day-left fish
 
-      // Test
-      console.log(`After day ${count + 1}:`, fishByDaysLeft);
+      console.log(`After day ${count + 1}:`, fishCounts);
     }
 
-    // Calculate the total population after the iterations
-    const population = fishByDaysLeft.reduce((sum, count) => sum + count, 0);
-    
+    const population = fishCounts.reduce((sum, count) => sum + count, 0);
     console.log(`After ${iterations} days, population is: ${population}`);
-    resolve(fishByDaysLeft);
+    resolve(fishCounts);
   });
 };
 
-dayCounter(testPond, 6).then(finalArray => {
-  console.log(finalArray)
+// Method for counting fish based on their days left
+const countFishByDaysLeft = (array) => {
+  const fishByDaysLeft = new Array(9).fill(0);
+  array.forEach(num => {
+    fishByDaysLeft[num]++;
+  });
+  return fishByDaysLeft;
+};
+
+// Load initial fish populations
+const everyFish = loadInput();
+const testPond = [1, 0, 6, 5];
+
+// Simulate fish population for different scenarios
+simulateFishPopulation(everyFish, 80, countFishByDaysLeft).then(finalArray => {
+  console.log(`There are ${finalArray.length} lanternfish after 80 days`);
+});
+
+simulateFishPopulation(testPond, 6, countFishByDaysLeft).then(finalArray => {
+  console.log(finalArray);
 });
