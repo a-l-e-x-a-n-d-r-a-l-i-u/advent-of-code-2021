6287e03ccf92cbe91333c716098e12a9268413bf
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2024-10-12 08:14:31 +1100

    Fatal error cause I'm changing the array while looping through it

--- day-6/makeChunks.ts
+++ day-6/makeChunks.ts
@@ -1,4 +1,5 @@
-export const splitArray = (array: number[], chunkSize: number): number[][] => {
+export const splitArray = (array: number[], numChunks: number): number[][] => {
+  const chunkSize = Math.ceil(array.length / numChunks); // Math.ceil() rounds up fractios to the nearest whole number, so 10/3 will have an chunk size of 4
   const chunks: number[][] = []; // [[1,2,3,4], [5,6,7,8], [9,10]
   for (let i = 0; i < array.length; i += chunkSize) {
     chunks.push(array.slice(i, i + chunkSize)); // Basically slicing it by array.slice(0, 4), array.slice(4, 8), array.slice(8, 12)
@@ -9,10 +10,10 @@ export const splitArray = (array: number[], chunkSize: number): number[][] => {
 export const dayCounter = (array: number[], iterations: number): Promise<number[]> => {
   return new Promise((resolve) => {
     for (let count = 0; count < iterations; count++) {
-      array.forEach(num => {
-        if (num === 0) { array.push(9) }  // Push a new fish for each 0
-    })
-
+      const zeroCount = array.filter(num => num === 0).length; // Count how many zeros
+for (let i = 0; i < zeroCount; i++) {
+    array.push(9); // Push 9 for each zero found
+}
     array = array.map(num => (num === 0 ? 6 : num - 1)); // Decrement or reset to 6
 
     }
