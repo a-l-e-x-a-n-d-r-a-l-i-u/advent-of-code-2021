c076dacc2f02a7c84746953837b66c124bd905d8
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2024-10-12 12:27:22 +1100

    Test as you go

--- day-7/part1.ts
+++ day-7/part1.ts
@@ -1,9 +1,17 @@
 import { CrabArmy } from './CrabArmy.js'
 import { loadInput } from './load.js'
 
-const input = loadInput()
+const initialPositions = loadInput()
+console.log(initialPositions) // Input: [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+const target = 3;
+const fuelUnitsTravelled = initialPositions.map(num => Math.abs(num - target));
+console.log(fuelUnitsTravelled) // Output: [13, 2, 1, 3, 1, 1, 4, 2, 1, 11]
+const totalFuelUsed = fuelUnitsTravelled.reduce((acc, curr) => acc + curr, 0);
+console.log(totalFuelUsed)
 
-const army = new CrabArmy(input)
+
+// Frank's code starts here
+const army = new CrabArmy(initialPositions)
 
 //lets just brute force for now
 let minimumFuelUse = Number.POSITIVE_INFINITY
