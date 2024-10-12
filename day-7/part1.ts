b630268dad70b24bcce61fc09d0a07370fbffb00
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2024-10-12 13:22:39 +1100

    I know the median starting position is optimal for fuel consumption. But why??

--- day-7/part1.ts
+++ day-7/part1.ts
@@ -2,19 +2,20 @@ import { CrabArmy } from './CrabArmy.js'
 import { loadInput } from './load.js'
 
 const initialPositions = loadInput()
+console.log(`Initial positions: ${initialPositions}`)
 
 const calculateFuelUsedPerCrab = (positions: number[], target: number): number[] => {
   return positions
-    .map(num => Math.abs(num - target)) // Calculate fuel units consumed from each starting position to get to target
+    .map(num => Math.abs(num - target)) // Absolute values
 };
 
 const calculateTotalFuelUsed = (positions: number[], target: number): number => {
   const fuelUsedPerCrab = calculateFuelUsedPerCrab(positions, target);
-  return fuelUsedPerCrab.reduce((acc, curr) => acc + curr, 0); // Sum up fuel for all positions
+  console.log(`Fuel used per crab: ${fuelUsedPerCrab}`)
+  return fuelUsedPerCrab.reduce((acc, curr) => acc + curr, 0);
 };
 
-console.log(calculateTotalFuelUsed(initialPositions, 3))
-
+console.log(`Total fuel used: ${calculateTotalFuelUsed(initialPositions, 0)}`)
 
 // Frank's code starts here
 const army = new CrabArmy(initialPositions)
