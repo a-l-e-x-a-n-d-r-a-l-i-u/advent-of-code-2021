3217097935029426b214dff2189c3b60f075cf12
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2024-10-12 17:31:27 +1100

    Brute forcing works, but this is mathematically elegant :D

--- day-7/part1.ts
+++ day-7/part1.ts
@@ -9,36 +9,22 @@ const calculateFuelUsedPerCrab = (positions: number[], target: number): number[]
     .map(num => Math.abs(num - target)) // Absolute values
 };
 
+const findMedian = (numbers: number[]): number => {
+  const sortAscending = [...numbers].sort((a, b) => a - b);
+  const mid = Math.floor(sortAscending.length / 2); // Round it down so it becomes your index
+  return sortAscending.length % 2 !== 0
+    ? sortAscending[mid] // If odd, return element ar midpoint
+    : (sortAscending[mid - 1] + sortAscending[mid]) / 2; // If even, return the average of the two middle elements
+};
+
 const calculateTotalFuelUsed = (positions: number[], target: number): number => {
   const fuelUsedPerCrab = calculateFuelUsedPerCrab(positions, target);
   console.log(`Fuel used per crab: ${fuelUsedPerCrab}`)
   return fuelUsedPerCrab.reduce((acc, curr) => acc + curr, 0);
 };
 
-console.log(`Total fuel used: ${calculateTotalFuelUsed(initialPositions, 0)}`)
-
-const findMedian = (numbers: number[]): number => {
-  const sortAscending = [...numbers].sort((a, b) => a - b);
-  const median = Math.floor(sortAscending.length / 2);
-  return sortAscending.length % 2 !== 0
-    ? sortAscending[median] // If odd, return the middle element
-    : (sortAscending[median - 1] + sortAscending[median]) / 2; // If even, return the average of the two middle elements
-};
-
-
-// Frank's code starts here
-const army = new CrabArmy(initialPositions)
-
-//lets just brute force for now
-let minimumFuelUse = Number.POSITIVE_INFINITY
-let bestPosition: number | null = null
-for (let testPosition = army.minimumPosition; testPosition <= army.maximumPosition; testPosition += 1) {
-  const fuelUse = army.naiveFuelToMoveToPosition(testPosition)
-  if (fuelUse < minimumFuelUse) {
-    minimumFuelUse = fuelUse
-    bestPosition = testPosition
-  }
-}
-
-console.log('fuel use', minimumFuelUse)
-console.log('best position', bestPosition)
+// Store variables
+const idealTarget: number = findMedian(initialPositions)
+console.log(`Ideal target: ${idealTarget}`)
+const totalFuelUsed: number = calculateTotalFuelUsed(initialPositions, idealTarget)
+console.log(`Total fuel used: ${totalFuelUsed}`)
