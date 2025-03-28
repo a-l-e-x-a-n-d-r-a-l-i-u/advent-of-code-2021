30f1f1a695c9ec4a2503c06de574189d5533336e
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2025-03-28 12:06:11 +1100

    Frank's OOP is scalable, but I prefer functional cleanliness

--- day-9/load.ts
+++ day-9/load.ts
@@ -3,6 +3,6 @@ import { readFileSync } from 'fs'
 export function loadInput(): number[][] {
   return readFileSync('input', { encoding: 'utf-8' })
     .split('\n')
-    .filter((line, _, array) => line.length === array[0].length)
+    .filter((line, array) => line.length === array[0].length) // The map should be rectangular
     .map((line) => line.trim().split('').map(Number))
 }
--- day-9/part1.ts
+++ day-9/part1.ts
@@ -1,35 +1,16 @@
-import { Grid, Point } from './Grid.js'
 import { loadInput } from './load.js'
 
 // Gradient descent is flaws because it doesn't consider local vs global minima
 
 /*
 
-Algorithm to Find Local Minima
-Iterate over each element in the grid.
-Compare it with its adjacent neighbours (up, down, left, right).
-If it's smaller than all its neighbours, it's a local minimum.
-**/
+Algorithm to Find Local Minima:
 
+1) Iterate over each element in the grid.
+2) Compare it with its adjacent neighbours (up, down, left, right).
+3) If it's smaller than all its neighbours, it's a local minimum.
 
-console.log(findLocalMinima(grid));
-
+*/
 
 const input = loadInput()
 
-const grid = new Grid(input)
-
-let totalRiskLevel = 0
-for (let x = 0; x <= grid.maxGridX; x += 1) {
-  for (let y = 0; y <= grid.maxGridY; y += 1) {
-    const currentPoint: Point = [x, y]
-    const currentValue = grid.valueAtPoint(currentPoint)
-    const neighbors = grid.findNeighbors(currentPoint)
-    const localMinimum = neighbors.every((neighbor) => grid.valueAtPoint(neighbor) > currentValue)
-    if (localMinimum) {
-      totalRiskLevel += currentValue + 1
-    }
-  }
-}
-
-console.log('total risk level', totalRiskLevel)
