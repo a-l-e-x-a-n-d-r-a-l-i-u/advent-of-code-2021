d50d6d66dd27d21cb6ffc070a340ded8ab84fc40
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2025-03-28 12:33:55 +1100

    How about now

--- day-9/part1.ts
+++ day-9/part1.ts
@@ -8,75 +8,30 @@ import { loadInput } from './load.js'
 
 */
 
-const input = loadInput()
-type Point = [number, number]; // this is a tuple!
+const grid = loadInput()
 
-const findNeighbors = (grid: number[][], point: Point): Point[] => {
-  const [x, y] = point;
-  const neighbors: Point[] = [];
+function findLocalMinima(grid: number[][]): [number, number, number][] {
+    const rows = grid.length;
+    const cols = grid[0].length;
+    const localMinima: [number, number, number][] = [];
 
-  if (x > 0) neighbors.push([x - 1, y]); // Left
-  if (x < grid[0].length - 1) neighbors.push([x + 1, y]); // Right
-  if (y > 0) neighbors.push([x, y - 1]); // Up
-  if (y < grid.length - 1) neighbors.push([x, y + 1]); // Down
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            const val = grid[i][j];
+            const neighbors: number[] = [];
 
-  return neighbors;
-};
+            if (i > 0) neighbors.push(grid[i - 1][j]); // Up
+            if (i < rows - 1) neighbors.push(grid[i + 1][j]); // Down
+            if (j > 0) neighbors.push(grid[i][j - 1]); // Left
+            if (j < cols - 1) neighbors.push(grid[i][j + 1]); // Right
 
-const compareWithNeighbors = (
-  grid: number[][], 
-  point: Point, 
-  compareFn: (value: number, neighbor: number) => boolean
-): boolean => {
-  const value = grid[point[1]][point[0]];
-  const neighbors = findNeighbors(grid, point);
-  
-  return neighbors.every(([nx, ny]) => compareFn(value, grid[ny][nx]));
-};
-
-// A flexible function that checks if a point is a local minimum
-const isLocalMinimum = (grid: number[][], point: Point): boolean => {
-  return compareWithNeighbors(grid, point, (value, neighbor) => value < neighbor);
-};
-
-// A flexible function that checks if a point is a local maximum
-const isLocalMaximum = (grid: number[][], point: Point): boolean => {
-  return compareWithNeighbors(grid, point, (value, neighbor) => value > neighbor);
-};
-
-// Find local minima or maxima (generalized function)
-const findExtrema = (
-  grid: number[][], 
-  checkFn: (grid: number[][], point: Point) => boolean
-): [number, number, number][] => {
-  const rows = grid.length;
-  const cols = grid[0].length;
-  const extrema: [number, number, number][] = [];
-
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      const point: Point = [j, i];
-      if (checkFn(grid, point)) {
-        extrema.push([j, i, grid[i][j]]);
-      }
+            if (neighbors.every(neighbor => val < neighbor)) {
+                localMinima.push([i, j, val]);
+            }
+        }
     }
-  }
 
-  return extrema;
-};
+    return localMinima;
+}
 
-// Example grid
-const grid: number[][] = [
-  [2, 1, 9, 9, 9, 4, 3, 2, 1, 0],
-  [3, 9, 8, 7, 8, 9, 4, 9, 2, 1],
-  [9, 8, 5, 6, 7, 8, 9, 8, 9, 2],
-  [8, 7, 6, 7, 8, 9, 6, 7, 8, 9],
-  [9, 8, 9, 9, 9, 6, 5, 6, 7, 8]
-];
-
-// Running the functions to get local minima and maxima
-const localMinima = findExtrema(grid, isLocalMinimum);
-const localMaxima = findExtrema(grid, isLocalMaximum);
-
-console.log("Local Minima:", localMinima);
-console.log("Local Maxima:", localMaxima);
+console.log(findLocalMinima(grid));
